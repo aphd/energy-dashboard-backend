42529d4d7133a2388ac326043b843e4d834200a3
antonio <antonio@antonios-MacBook-Pro.local>
2024-09-06 12:03:56 +0200

    refactor

--- src/get-balance.js
+++ src/get-balance.js
@@ -1,22 +1,19 @@
 const { Web3 } = require('web3');
 require('dotenv').config();
 const NETWORK = process.env.ETHEREUM_NETWORK;
-const address = process.env.FROM || ``;
+const address = process.env.FROM;
 
 const getEtherBalance = async () => {
     // Configuring the connection to an Ethereum node
+    if (!address) throw new Error('Environment variable FROM is not set');
     const web3 = new Web3(new Web3.providers.HttpProvider(`https://${NETWORK}.infura.io/v3/${process.env.INFURA_API_KEY}`)); // prettier-ignore
-
-    try {
-        const balance = await web3.eth.getBalance(address);
-        const ethBalance = web3.utils.fromWei(balance, 'ether')
-        console.log(`The balance of address ${address} is: ${ethBalance} ETH`);
-        return +ethBalance;
-    } catch (error) {
-        console.error('An error occurred while fetching the balance:', error);
-    }
-}
+    const balance = await web3.eth.getBalance(address);
+    const ethBalance = web3.utils.fromWei(balance, 'ether');
+    console.log(`The balance of address ${address} is: ${ethBalance} ETH`);
+    return +ethBalance;
+};
 
 if (require.main === module) getEtherBalance();
 
-module.exports = {getEtherBalance};
+module.exports = { getEtherBalance };
+
