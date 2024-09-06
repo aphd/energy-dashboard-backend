f2fc2bb2d7c96da3f04b7030297b51391ea04e9c
antonio <antonio@antonios-MacBook-Pro.local>
2024-09-06 11:42:25 +0200

    test getEtherBalance function

--- src/get-balance.js
+++ src/get-balance.js
@@ -1,17 +1,17 @@
 const { Web3 } = require('web3');
 require('dotenv').config();
 const NETWORK = process.env.ETHEREUM_NETWORK;
-const address = process.env.FROM;
+const address = process.env.FROM || ``;
 
 const getEtherBalance = async () => {
     // Configuring the connection to an Ethereum node
     const web3 = new Web3(new Web3.providers.HttpProvider(`https://${NETWORK}.infura.io/v3/${process.env.INFURA_API_KEY}`)); // prettier-ignore
 
     try {
-        // Fetching the balance of the specified address
         const balance = await web3.eth.getBalance(address);
-        console.log(`The balance of address ${address} is: ${web3.utils.fromWei(balance, 'ether')} ETH`);
-        return balance;
+        const ethBalance = web3.utils.fromWei(balance, 'ether')
+        console.log(`The balance of address ${address} is: ${ethBalance} ETH`);
+        return +ethBalance;
     } catch (error) {
         console.error('An error occurred while fetching the balance:', error);
     }
