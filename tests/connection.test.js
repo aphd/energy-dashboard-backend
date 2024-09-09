f0eec2cfe9435d5bfec663c4e095901af99d9fd9
antonio <antonio@antonios-MacBook-Pro.local>
2024-09-09 13:15:05 +0200

    remove comment

--- tests/connection.test.js
+++ tests/connection.test.js
@@ -5,7 +5,6 @@ const {testInfuraConnection} = require('../src/connection'); // Adjust the path 
 describe('Connection', () => {
     test('Verify that the web interface successfully connects to the blockchain network.', async () => {
         const block = await testInfuraConnection();
-		console.log(`message: ${block}`);
         expect(typeof block).toBe('bigint');
     });
 });
